0800799b54847092d77212e0404034b93e2583bf
Jared <jared@interlacedpixel.com>
2023-02-03 22:52:27 -0600

    login window

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace MapleSeedX.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public MainWindowViewModel()
+    {
+        Username = string.Empty;
+        Userpass = string.Empty;
+    }
+
+    public string Greeting => "Welcome to MapleSeed X!";
+    
+    public string Username { get; set; }
+    
+    public string Userpass { get; set; }
+    
+    public bool Autologin { get; set; }
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MapleSeedX.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
